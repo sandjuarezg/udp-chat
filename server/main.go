7ea35d517ea0065d32ad329b8467763b9e522416
Sandra Juarez <sandjuarezg@gmail.com>
2022-03-06 18:31:33 -0600

    refactor: improve code

--- server/main.go
+++ server/main.go
@@ -29,64 +29,38 @@ func main() {
 
 	reply := make([]byte, 1024)
 
-	n, addr, err := conn.ReadFromUDP(reply)
-	if err != nil {
-		log.Fatal(err)
-	}
-
 	for {
+		n, addr, err := conn.ReadFromUDP(reply)
+		if err != nil {
+			log.Fatal(err)
+		}
 
-		name := reply[:n-1]
-
-		addrs = append(addrs, addr)
-		fmt.Println(string(name), "connected")
-
-		mess := fmt.Sprintf(" - %s connected - \n", name)
-		mess += fmt.Sprintf(" - %d connected users - \n", len(addrs))
-
-		for _, element := range addrs {
-			_, err = conn.WriteTo([]byte(mess), element)
-			if err != nil {
-				log.Fatal(err)
+		if addrExists(addr) {
+			for _, element := range addrs {
+				if element.String() != addr.String() {
+					_, err = conn.WriteTo([]byte(fmt.Sprintf("-> %s", reply[:n])), element)
+					if err != nil {
+						log.Fatal(err)
+					}
+				}
 			}
-		}
+		} else {
+			addrs = append(addrs, addr)
 
-		addrAux, nAux, err := handleRequest(conn, reply)
-		if err != nil {
-			log.Fatal(err)
-		}
+			name := reply[:n-1]
+			fmt.Println(string(name), "connected")
 
-		addr = addrAux
-		n = nAux
-	}
-}
+			mess := fmt.Sprintf(" - %s connected - \n", name)
+			mess += fmt.Sprintf(" - %d connected users - \n", len(addrs))
 
-// handleRequest Handle client request
-//  @param1 (conn): connection between client and server
-//  @param2 (reply): buffer of reply
-//
-//  @return1 (addrAux): address aux of a UDP end point
-//  @return2 (n): number of characters in buffer
-//  @return3 (err): error variable
-func handleRequest(conn *net.UDPConn, reply []byte) (addrAux *net.UDPAddr, n int, err error) {
-	for {
-		n, addrAux, err = conn.ReadFromUDP(reply)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		if !addrExists(addrAux) {
-			return
-		}
-
-		for _, element := range addrs {
-			if element.String() != addrAux.String() {
-				_, err = conn.WriteTo([]byte(fmt.Sprintf("-> %s", reply[:n])), element)
+			for _, element := range addrs {
+				_, err = conn.WriteTo([]byte(mess), element)
 				if err != nil {
 					log.Fatal(err)
 				}
 			}
 		}
+
 	}
 }
 
